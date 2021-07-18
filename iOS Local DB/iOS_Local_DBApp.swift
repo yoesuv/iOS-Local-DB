675228dafbca07d56cedbaf41a0c5f90aac45502
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2021-07-19 04:46:24 +0700

    Initial Commit

---
+++ iOS Local DB/iOS_Local_DBApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Local_DBApp.swift
+//  iOS Local DB
+//
+//  Created by Yusuf Saifudin on 19/07/21.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Local_DBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
