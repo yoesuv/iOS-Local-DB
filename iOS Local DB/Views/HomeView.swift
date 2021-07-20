cbb278862fc2f38ca21469aa40f80dd185097d62
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2021-07-20 10:06:22 +0700

    :newspaper: add home view

---
+++ iOS Local DB/Views/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  iOS Local DB
+//
+//  Created by Yusuf Saifudin on 20/07/21.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Home View")
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
