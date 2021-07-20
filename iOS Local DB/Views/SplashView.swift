bb2bc9303ef6e18e7bead5e7e6190ef2813a30c5
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2021-07-20 10:02:02 +0700

    :newspaper: add splash

---
+++ iOS Local DB/Views/SplashView.swift
@@ -0,0 +1,22 @@
+//
+//  SplashView.swift
+//  iOS Local DB
+//
+//  Created by Yusuf Saifudin on 20/07/21.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text("iOS Local DB")
+            .font(.title)
+            .fontWeight(.semibold)
+    }
+}
+
+struct SplashView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashView()
+    }
+}
