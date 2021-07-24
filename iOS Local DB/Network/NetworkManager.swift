e3745a5d0175cef8bbd5d7f685492fa41ebb73e5
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2021-07-24 08:49:15 +0700

    :construction: initialize db directory

--- iOS Local DB/Network/NetworkManager.swift
+++ iOS Local DB/Network/NetworkManager.swift
@@ -7,14 +7,28 @@
 
 import Foundation
 import Alamofire
+import ObjectBox
 
 class NetworkManager: ObservableObject {
+    
+    let dbName = "db_users"
     
     func fetchUsers() {
         AF.request("http://jsonplaceholder.typicode.com/users/", method: .get)
             .responseDecodable(of: [User].self) { response in
                 if (response.error == nil) {
-                    print("NetworkManager # success \(response.value?.count)")
+                    if let count = response.value?.count {
+                        print("NetworkManager # success \(count)")
+                    }
+                    do {
+                        let appSupport = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+                            .appendingPathComponent(Bundle.main.bundleIdentifier!)
+                        let directory = appSupport.appendingPathComponent(self.dbName)
+                        try FileManager.default.createDirectory(at: directory, withIntermediateDirectories: true, attributes: nil)
+                        let store = try Store(directoryPath: directory.path)
+                    } catch {
+                        print("NetworkManager # error \(error)")
+                    }
                 } else {
                     print("NetworkManager # error \(response.error!)")
                 }
